6c058df51ae893c33959ba92ad2814de72633aec
nomorechokedboy <haiduongit@protonmail.com>
2022-01-17 22:43:56 +0700

    update pre push

--- src/middleware/auth/isAdmin.ts
+++ src/middleware/auth/isAdmin.ts
@@ -1,5 +1,5 @@
-import jwt, { Jwt, JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { SECRET_KEY } from '../../config/env';
 
 interface UserPayload {
